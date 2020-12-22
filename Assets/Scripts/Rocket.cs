ae286f087e575f9d7f888c804ec34fe434adc207
Robert Thiel <33242830+devBert7@users.noreply.github.com>
2020-12-22 14:59:18 -0700

    Setting up Thrusters and turning capabilites

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    void Update() {
+        ProcessInput();
+    }
+
+    void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            print("Thrust");
+        }
+        
+        if (Input.GetKey(KeyCode.A)) {
+            print("Rotating Left");
+        } else if (Input.GetKey(KeyCode.D)) {
+            print("Rotating Right");
+        }
+    }
+}

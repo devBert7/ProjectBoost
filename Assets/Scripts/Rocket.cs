794394298c8238d31fc3457634a3dd07a1cbca68
Robert Thiel <33242830+devBert7@users.noreply.github.com>
2021-01-19 15:55:09 -0700

    Added audio source component and audio file
    Changed from play on awake to a scripted play which listens for the spacebar and starts the audio and stops it when the key is lifted.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
     void Update() {
         ProcessInput();
     }
 
     void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            print("Thrust");
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
         }
-        
+
         if (Input.GetKey(KeyCode.A)) {
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }

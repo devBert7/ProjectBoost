a14ad7bdfb6c567673b4f83ae19ad06e9f781167
Robert Thiel <33242830+devBert7@users.noreply.github.com>
2021-01-31 11:57:34 -0700

    changes
    I did this
    I also did that

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,33 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-    Rigidbody rigidbody;
-    AudioSource audioSource;
+	[SerializeField] float rcsThrust = 250f;
+	[SerializeField] float mainThrust = 150f;
 
-    void Start() {
-        rigidbody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-    }
-    void Update() {
-        ProcessInput();
-    }
+	Rigidbody rigidBody;
+	AudioSource audioSource;
 
-    void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) {
-            rigidbody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying) {
-                audioSource.Play();
-            }
-        } else {
-            audioSource.Stop();
-        }
+	void Start() {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
 
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
-        }
-    }
+	void Update() {
+		Thrust();
+		Rotate();
+	}
+
+	void Thrust() {
+		float thrustThisFrame = mainThrust * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.Space)) {
+			rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+			if (!audioSource.isPlaying) {
+					audioSource.Play();
+			}
+		} else {
+			audioSource.Stop();
+		}
+	}
+
+	void Rotate() {
+		rigidBody.freezeRotation = true;
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward * rotationThisFrame);
+		} else if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
+		}
+
+		rigidBody.freezeRotation = false;
+	}
 }
